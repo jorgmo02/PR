191ced9d28a6e1f00e76d5e0189c8140a1b9ac2e
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2021-05-23 13:31:56 +0200

    practica casi terminada
    Co-Authored-By: Jorge Moreno Martínez <jorgemomartin@gmail.com>

--- Programación SMT/Programación SMT/Produccion_De_Alimentos.cs
+++ Programación SMT/Programación SMT/Produccion_De_Alimentos.cs
@@ -25,12 +25,63 @@ namespace Programacion_SMT
         int[] durezas;
         int[,] precios;
         string Produces(int i, int j) { return "Produces_" + i.ToString() + "_" + j.ToString(); }
-        string Compras(int i, int j) { return "Compras_" + i.ToString() + "_" + j.ToString(); }
-        string AceiteDisponible(int i, int j) { return "AceiteDisponible_" + i.ToString() + "_" + j.ToString(); }
+        string Produces() { return "Produces_"; }
+        string Compras(int i, int j) { return Compras() + i.ToString() + "_" + j.ToString(); }
+        string Compras() { return "Compras_"}
+        string AceiteDisponible(int i, int j) { return AceiteDisponible() + i.ToString() + "_" + j.ToString(); }
+        string AceiteDisponible() { return "AceiteDisponible_"; }
 
-        public  void ProduccionDeAlimentos()
+        public void ProduccionDeAlimentos()
         {
             LeeProduccionDeAlimentos();
+
+            // asserts  -------------------------------------------------
+            if (!(numAceitesV >= 0 &&
+                numAceitesN >= 0 &&
+                numMeses > 0 &&
+                VALOR > 0 &&
+                MAXV > 0 &&
+                MAXN > 0 &&
+                MinD > 0 &&
+                MaxD > MinD &&
+                MinB >= 0
+            ))
+                Console.WriteLine("ERROR: assertion failed. Check input.");
+
+            foreach (int i in MCAP)
+                if (!(i > 0))
+                {
+                    Console.WriteLine("ERROR: assertion failed. Check input at MCAP.");
+                    return;
+                }
+            foreach (int i in ton)
+                if (!(i > 0))
+                {
+                    Console.WriteLine("ERROR: assertion failed. Check input at ton.");
+                    return;
+                }
+            foreach (int i in durezas)
+                if (!(i > 0))
+                {
+                    Console.WriteLine("ERROR: assertion failed. Check input at dur.");
+                    return;
+                }
+            foreach (int i in CA)
+                if (!(i > 0))
+                {
+                    Console.WriteLine("ERROR: assertion failed. Check input at CA.");
+                    return;
+                }
+            foreach (int i in precios)
+                if (!(i > 0))
+                {
+                    Console.WriteLine("ERROR: assertion failed. Check input at precios.");
+                    return;
+                }
+            // fin asserts  -------------------------------------------------            
+
+
+
             List<string> lines = new List<string> {
                  ProduceModels(),
                 SetLogic("QF_LRA")
@@ -39,7 +90,7 @@ namespace Programacion_SMT
             // declaracion variables
             for (int i = 1; i <= numMeses; i++)
             {
-                for (int j = 0; j < numAceitesTotales; j++)
+                for (int j = 1; j <= numAceitesTotales; j++)
                 {
                     lines.Add(intvar(Produces(i, j)));
                     lines.Add(intvar(Compras(i, j)));
@@ -48,8 +99,120 @@ namespace Programacion_SMT
             }
 
             //constraint forall(m in 1..numMeses)
- //(sum(j in 1..numAceitesTotales)(durezas[j] * Produces[m, j]) <= MaxD * (sum(j in 1..numAceitesTotales)(Produces[m, j])) /\ sum(j in 1..numAceitesTotales)
-      //(durezas[j] * Produces[m, j]) >= MinD * (sum(j in 1..numAceitesTotales)(Produces[m, j])));
+            //(sum(j in 1..numAceitesTotales)(durezas[j] * Produces[m, j]) <= MaxD * (sum(j in 1..numAceitesTotales)(Produces[m, j])) /\ sum(j in 1..numAceitesTotales)
+            //(durezas[j] * Produces[m, j]) >= MinD * (sum(j in 1..numAceitesTotales)(Produces[m, j])));
+            List<int> sumatorio = new List<int>();
+            for (int i = 0; i < numMeses; i++)
+                for (int j = 0; j < numAceitesTotales; j++)
+                    sumatorio.Add(durezas[j]);
+
+            addassert(addle(addsumOperacion(sumatorio, Produces(), "*", numMeses, numAceitesTotales),
+                addmul(MaxD.ToString(), addSumVar(Produces(), numMeses, numAceitesTotales))));
+
+            addassert(addge(addsumOperacion(sumatorio, Produces(), "*", numMeses, numAceitesTotales),
+                addmul(MinD.ToString(), addSumVar(Produces(), numMeses, numAceitesTotales))));
+
+
+            //constraint(sum(m in 1..numMeses, a in 1..numAceitesTotales)
+            //((Produces[m, a] * VALOR) - (Compras[m, a] * precios[m, a] + CA[m, a] * AceiteDisponible[m, a]))) >= MinB;
+            List<int> sumatorioValor = new List<int>();
+            for (int i = 1; i <= numMeses; i++)
+                for (int j = 1; j <= numAceitesTotales; j++)
+                    sumatorioValor.Add(VALOR);
+
+            List<int> sumatorioPrecios = new List<int>();
+            for (int i = 1; i <= numMeses; i++)
+                for (int j = 1; j <= numAceitesTotales; j++)
+                    sumatorioPrecios.Add(precios[i, j]);
+
+            List<int> sumatorioCA = new List<int>();
+            for (int i = 1; i <= numMeses; i++)
+                for (int j = 1; j <= numAceitesTotales; j++)
+                    sumatorioCA.Add(CA[i, j]);
+
+            addassert(addge(addminus(addsumOperacion(sumatorioValor, Produces(), "*", numMeses, numAceitesTotales),
+                addplus(addsumOperacion(sumatorioPrecios, Compras(), "*", numMeses, numAceitesTotales),
+                addsumOperacion(sumatorioCA, AceiteDisponible(), "*", numMeses, numAceitesTotales))), MinB.ToString()));
+
+
+            // % 3. Al final del año tienen que quedar "toneladas" toneladas de aceite
+            // constraint forall(a in 1..numAceitesTotales) (AceiteDisponible[numMeses,a] = toneladas[a]);
+            for (int i = 1; i <= numAceitesTotales; i++)
+            {
+                addassert(
+                    addeq(AceiteDisponible(numMeses, i).ToString(), toneladas[i - 1].ToString())
+                );
+            }
+
+            // % 4. cada mes nos queda lo que compramos menos lo que producimos más lo que teníamos almacenado anteriormente
+            // constraint forall(m in 2..numMeses,a in 1..numAceitesTotales) (AceiteDisponible[m,a]=Compras[m,a]-Produces[m,a] +AceiteDisponible[m-1,a]);
+            for (int i = 2; i <= numMeses; i++)
+            {
+                for (int j = 1; j <= numAceitesTotales; j++)
+                {
+                    addassert(
+                        addeq(
+                            AceiteDisponible(i, j).ToString(),
+                            addplus(
+                                addminus(Compras(i, j).ToString(), Produces(i, j).ToString()),
+                                AceiteDisponible(i - 1, j)
+                                )
+                            )
+                    );
+                }
+            }
+            // %El primer mes no contamos con lo que teníamos almacenado anteriormente
+            //constraint forall(a in 1..numAceitesTotales) (AceiteDisponible[1,a]=Compras[1,a]-Produces[1,a]);
+            for (int i = 1; i <= numAceitesTotales; i++)
+            {
+                addassert(addeq(AceiteDisponible(1, i), addminus(Compras(1, i), Produces(1, i))));
+            }
+
+            //% 5.cada mes produces como mucho MAXV aceites vegetales
+            //constraint forall(m in 1..numMeses)(
+            //sum(a in 1..numAceitesV)(Produces[m, a]) <= MAXV
+            //);
+
+            for (int i = 1; i <= numMeses; i++)
+                for (int j = 1; j <= numAceitesV; j++)
+                    addassert(addle(addSumVar(Produces(), numMeses, numAceitesV), MAXV.ToString()));
+
+            // % 6. cada mes produces como mucho MAXN aceites animales
+            // constraint forall(m in 1..numMeses)(
+            //     sum(a in numAceitesV..numAceitesV + numAceitesN)(Produces[m,a])  <= MAXN
+            // );
+            string myLine = "( + ";
+            for (int i = 1; i <= numMeses; i++)
+            {
+                for (int j = numAceitesV; j <= numAceitesV + numAceitesN; j++)
+                    myLine += Produces(i, j) + " ";
+            }
+            myLine += ")";
+            lines.Add(addle(myLine, MAXN.ToString()));
+
+            // % 7. En ningún mes se supera el almacenamiento disponible para cada aceite
+            // constraint forall(m in 1..numMeses,a in 1..numAceitesTotales)((AceiteDisponible[m,a])<=MCAP[a]);
+            for (int i = 1; i <= numMeses; i++)
+                for (int j = 1; j <= numAceitesTotales; j++)
+                    addassert(addle(AceiteDisponible(i, j), MCAP[j - 1].ToString()));
+
+            // % 8.Las compras tienen que ser positivas o 0
+            //constraint forall(m in 1..numMeses, a in 1..numAceitesTotales)(Compras[m, a] >= 0);
+            for (int i = 1; i < numMeses; i++)
+                for (int j = 0; j < numAceitesTotales; j++)
+                    addassert(addge(Compras(i, j), "0"));
+
+            // %9.Los aceites disponibles tienen que ser positivas o 0
+            // constraint forall(m in 1..numMeses, a in 1..numAceitesTotales)(AceiteDisponible[m,a]>=0);
+            for (int i = 1; i <= numMeses; i++)
+                for (int j = 1; j <= numAceitesTotales; j++)
+                    addassert(addge(AceiteDisponible(i, j), "0"));
+
+            //% 10.La producción tiene que ser positiva o 0
+            //constraint forall(m in 1..numMeses, a in 1..numAceitesTotales)(Produces[m, a] >= 0);
+            for (int i = 1; i <= numMeses; i++)
+                for (int j = 1; i <= numAceitesTotales; i++)
+                    addassert(addge(Produces(i, j), "0"));
 
 
             lines.Add(checksat());
@@ -78,6 +241,7 @@ namespace Programacion_SMT
 
             RellenaFichero(lines, "ProduccionDeAlimentos.smt2");
         }
+
         void LeeProduccionDeAlimentos()
         {
             StreamReader file = new StreamReader(inputFilePath);
@@ -136,5 +300,5 @@ namespace Programacion_SMT
             file.Close();
         }
     }
-}
+
 
--- Programación SMT/Programación SMT/SMT_Translator.cs
+++ Programación SMT/Programación SMT/SMT_Translator.cs
@@ -9,10 +9,12 @@ namespace Programacion_SMT
         static string filesPath = "D:\\Archivos\\Universidad\\3V\\PR\\SMTFiles\\";   // sustituir
         static void RellenaFichero(List<string> content, string path)
         {
-            try {
+            try
+            {
                 File.WriteAllLines(filesPath + path, content);
             }
-            catch {
+            catch
+            {
                 Console.WriteLine("ERROR: No se pudo rellenar el fichero.");
             }
         }
@@ -38,6 +40,7 @@ namespace Programacion_SMT
         static string addlt(string a1, string a2) { return "(< " + a1 + " " + a2 + " )"; }
         static string addgt(string a1, string a2) { return "(> " + a1 + " " + a2 + " )"; }
         static string addplus(string a1, string a2) { return "(+ " + a1 + " " + a2 + " )"; }
+        static string addminus(string a1, string a2) { return "(- " + a1 + " " + a2 + " )"; }
         static string addimply(string a1, string a2) { return "(=> " + a1 + " " + a2 + " )"; }
         static string addmul(string a1, string a2) { return "(* " + a1 + " " + a2 + " )"; }
         static string addassert(string a) { return "(assert " + a + " )"; }
@@ -45,10 +48,49 @@ namespace Programacion_SMT
         {
             if (a.Count == 0) return "0";
             if (a.Count == 1) return a[0].ToString();
-            int x = a[a.Count-1];
+            int x = a[a.Count - 1];
             a.RemoveAt(a.Count - 1);
 
             return "(+ " + x + " " + addsum(a) + " )";
+        }
+        public static string addSumVar(string variable, int i, int j)
+        {
+            return _addSumVar(variable, i, j, j);
+        }
+        private static string _addSumVar(string variable, int i, int j, int initJ)
+        {
+            if (i == 0 && j == 0) return "0";
+            if (i == 1) return variable + i.ToString();
+            if (i == 0 && j == 1) return variable + "0_1";
+            if (j == 0)
+                return "(+" + variable + i.ToString() + " " + _addSumVar(variable, i - 1, initJ, initJ) + ")";
+            return "(+" + variable + i.ToString() + " " + _addSumVar(variable, i, j - 1, initJ) + ")";
+
+        }
+        public static string addSumVar(string variable, int i)
+        {
+            if (i == 0) return "0";
+            if (i == 1) return variable + i.ToString();
+            return "(+" + variable + i.ToString() + " " + addSumVar(variable, i - 1) + ")";
+        }
+        public static string addsumOperacion(List<int> valor, string variable, string operacion, int i, int j)
+        { return _addsumOperacion(valor, variable, operacion, i, j, i, j); }
+        private static string _addsumOperacion(List<int> valor, string variable, string operacion, int i, int j, int SizeI, int SizeJ)
+        {
+            if (i == 0 && j == 0) return "0";
+            if (i == 0 && j == 1) return valor[0].ToString();
+            int x = valor[valor.Count - 1];
+            valor.RemoveAt(valor.Count - 1);
+            if (j == 0) return "(+ " + _addsumOperacion(valor, variable, operacion, i - 1, SizeJ, SizeI, SizeJ) + " (" + operacion + " " + x + " " + variable + i.ToString() + "_" + j.ToString() + " )";
+            else return "(+ " + _addsumOperacion(valor, variable, operacion, i, j - 1, SizeI, SizeJ) + " (" + operacion + " " + x + " " + variable + i.ToString() + "_" + j.ToString() + " )";
+        }
+        static string addsumOperacion(List<int> valor, string variable, string operacion, int i)
+        {
+            if (i == 0) return "0";
+            if (i == 1) return valor[0].ToString();
+            int x = valor[valor.Count - 1];
+            valor.RemoveAt(valor.Count - 1);
+            return "(+ " + addsumOperacion(valor, variable, operacion, i - 1) + " (" + operacion + " " + x + " " + variable + i.ToString() + " )";
         }
         static string checksat() { return "(check-sat)"; }
         static string getModel() { return "(get-model)"; }
